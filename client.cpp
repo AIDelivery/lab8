1a6d1a08dced8d9da16f0b57143a1dbdccea1927
Dan <daniyar.burakaev@mail.ru>
2019-12-15 23:08:49 +0300

    Ready...

--- client.cpp
+++ client.cpp
@@ -1,125 +1,60 @@
-#include <stdio.h>
-#include <mqueue.h>
-#include <sys/stat.h>
-#include <stdlib.h>
-#include <unistd.h>
-#include <time.h>
-#include <string.h>
-#include <iostream>
+#include "func.h"
 using namespace std;
 
-#define MAX_SIZE       256
-#define BUF_SIZE      8192
+int init_sleep(void);
+void signal_handler(int);
 
-struct msg_type {
-    time_t time;
-    string text;
-    pid_t sender_pid;
-};
+int main(int argc, char* argv[]) {
+    int msgq_id;
 
-int send_message (mqd_t msgq_id, string str_to_send) {
+    /* Параметры сообщения */
     msg_type msg;
-    timespec ts; ts.tv_sec = 1;
+
+    /* Установка реакции на сигнал от сервера */
+    signal(SIGINT, signal_handler);
     
-    msg.sender_pid = getpid();
-    msg.text = str_to_send;
-    msg.time = 0;
-    
-    int res = mq_send(msgq_id, (char*) &msg, sizeof(struct msg_type), 1);
-    
-    if (res < 0) {
-        return 0;
+    time_t timer = stoi(argv[1]);
+
+    //key = ftok("./", 'a');
+
+    /* Создание очереди с флагами
+        0644 - чтение/запись для владельца и чтение для членов группы и прочих пользователей
+        IPC_CREAT, IPC_EXCL - проверка на наличие очереди с тем же ключом
+    */
+    if ((msgq_id = msgget(KEY, IPC_CREAT)) == -1) {
+        perror("msgget");
+        exit(EXIT_FAILURE);
     }
     
-    cout << "[Server send] [" << msg.text << "] [" << msg.sender_pid << "]" << endl;
+    char b[] = "Hello";
+    int i;
     
-    return 1;
-}
-
-int read_message (mqd_t msgq_id, msg_type &msg_to_receive, unsigned int &sender) {
-    timespec ts; ts.tv_sec = 1;
-    int res = mq_receive(msgq_id, (char*) &msg_to_receive, sizeof(struct msg_type), &sender);
+    for (i = 0; i < timer; i++) {
+        printf("Посылка эхо-сигнала серверу: %d секунд из %d\n", i, (int) timer);
+        /* Сообщение серверу о запуске клиента */
+        send_message(msgq_id, &msg, 1, b, timer);
+        
+        sleep(1);
+        
+        /* Чтение сообщения от сервера */
+        if (read_message(msgq_id, &msg, 2) != -1)
+            break;
+    }
     
-    if (res < 0)
-        return 0;
+    if (i == timer)
+        printf("[Закрытие...]\n");
+    else {
+        printf("[Сигнал от сервера получен]\n");
+        
+        while (strcmp(msg.mtext, "exit()") != 0) {
+            printf("Введите сообщение для сервера: ");
+            scanf("%s", msg.mtext);
+            send_message(msgq_id, &msg, 4, msg.mtext, timer);
+            read_message(msgq_id, &msg, 3);
+        }
+    }
     
-    cout << "[Server read] [" << msg_to_receive.text << "] [" << msg_to_receive.sender_pid << "]" << endl;
-    
-    return 1;
-}
-
-
-int main(int argc, char* argv[])
-{
-
-        mqd_t msgq_id;
-        struct msg_type msg;
-        unsigned int sender;
-        struct mq_attr msgq_attr;
-        unsigned int prio = 1;
-
-        unsigned int recv_size = BUF_SIZE;
-        const char file[] = "/test1";
-
-        /* mq_open() for opening an existing queue */
-        msgq_id = mq_open(file, O_RDWR | O_EXCL);
-        if (msgq_id < 0) {
-                perror("mq_open");
-                exit(1);
-        }
-
-        /* getting the attributes from the queue        --  mq_getattr() */
-        if (mq_getattr(msgq_id, &msgq_attr) == -1) {
-                perror("mq_getattr");
-                exit(1);
-        }
-
-        msgq_attr.mq_curmsgs = 0;
-        mq_setattr(msgq_id, &msgq_attr, NULL);
-
-        printf("        Queue: %d\n\
-        stores at most %ld messages\n\
-        large at most %ld bytes each\n\
-        currently holds %ld messages\n",
-        msgq_id, msgq_attr.mq_maxmsg, msgq_attr.mq_msgsize, msgq_attr.mq_curmsgs);
-
-
-        if (recv_size < msgq_attr.mq_msgsize) { recv_size = msgq_attr.mq_msgsize; }
-
-        time_t time_awaiting = stoi(argv[1]);
-        if(!send_message(msgq_id, "cc"))
-        {
-            perror("mq_send");
-            exit(1);
-        }
-
-        int time_consumed;
-
-        for (time_consumed = 0; time_consumed < time_awaiting; time_consumed++) {
-            if (read_message(msgq_id, msg, sender))
-                break;
-
-            sleep(1);
-            send_message(msgq_id, "cc");
-        }
-
-        if (time_consumed == time_awaiting) {
-            cout << "[Server?]" << endl;
-            mq_unlink(file);
-            mq_close(msgq_id);
-            exit(EXIT_FAILURE);
-        }
-
-        cout << endl << "[MQ-Connection established]" << endl;
-        mq_unlink(file);
-        mq_close(msgq_id);
-        exit(EXIT_SUCCESS);
-
-//         while(strcmp(msg.buf, "exit()") != 0) {
-//             cout << "Message: ";
-//             cin >> msg.buf;
-//             mq_send(msgq_id, (char*)&msg, sizeof(struct msg_type_s), prio);
-//         }
-
-        return 0;
+    /* Уничтожение очереди */
+    msgctl(msgq_id, IPC_RMID, 0);
+    exit(EXIT_SUCCESS);
 }
--- server.cpp
+++ server.cpp
@@ -1,132 +1,50 @@
-#include <stdio.h>
-#include <mqueue.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <string.h>
-
-#include <iostream>
+#include "func.h"
 using namespace std;
 
-#define FILE_MODE (S_IRUSR|S_IWUSR|S_IRGRP|S_IROTH)
-
-#define MAXSIZE     256
-#define BUFFER      8192
-
-struct msg_type {
-    time_t time;
-    string text;
-    pid_t sender_pid;
-};
-
-int send_message (mqd_t msgq_id, string str_to_send) {
-    msg_type msg;
-    timespec ts; ts.tv_sec = 20000000;
-    
-    msg.sender_pid = getpid();
-    msg.text = str_to_send;
-    msg.time = 0;
-    
-    int res = mq_send(msgq_id, (char*) &msg, sizeof(struct msg_type), 1);
-    
-    if (res < 0) {
-        return 0;
-    }
-    
-    cout << "[Server send] [" << msg.text << "] [" << msg.sender_pid << "]" << endl;
-    
-    return 1;
-}
-
-int read_message (mqd_t msgq_id, msg_type &msg_to_receive, unsigned int &sender) {
-//     timespec ts; ts.tv_nsec = 20000000;
-    int res = mq_receive(msgq_id, (char*) &msg_to_receive, sizeof(struct msg_type), &sender);
-    
-    if (res < 0)
-        return 0;
-    
-    cout << "[Server read] [" << msg_to_receive.text << "] [" << msg_to_receive.sender_pid << "]" << endl;
-    
-    return 1;
-}
-
 int main(int argc, char* argv[]) {
-    time_t time_awaiting = stoi(argv[1]);
-    mqd_t msgq_id;
-    struct mq_attr msgq_attr;
-    unsigned int prio = 1;
-    char queue_path[] = "/test1";
-    unsigned int recv_size = 8192;
-
-    msgq_id = mq_open(queue_path, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG, NULL);
-    if(msgq_id < 0) {
-            perror("mq_open");
-            exit(1);
-    }
-
-    mq_getattr(msgq_id, &msgq_attr);
-    msgq_attr.mq_curmsgs = 0;
-
-    printf("        Queue: %d\n\
-    stores at most %ld messages\n\
-    large at most %ld bytes each\n\
-    currently holds %ld messages\n",
-    msgq_id, msgq_attr.mq_maxmsg, msgq_attr.mq_msgsize, msgq_attr.mq_curmsgs);
-
-    mq_setattr(msgq_id, &msgq_attr, NULL);
-
-    if (!send_message(msgq_id, "dd")) {
-        cout << "[Unexpected error]" << endl;
-        mq_unlink(queue_path);
-        mq_close(msgq_id);
-        exit(EXIT_FAILURE);
-    }
-
-    time_t time_consumed;
+    int msgq_id = msgget(KEY, (0644 | IPC_CREAT));
     msg_type msg;
-    unsigned int sender;
+    int timer = stoi(argv[1]);
 
-    for (time_consumed = 0; time_consumed < time_awaiting; time_consumed++) {
-        if (read_message(msgq_id, msg, sender))
-            break;
-        mq_getattr(msgq_id, &msgq_attr);
-        cout << "curmes: " << msgq_attr.mq_curmsgs << endl;
-        sleep(1);
-        send_message(msgq_id, "dd");
-    }
-
-    if(time_consumed >= time_awaiting) {
-        cout << "[Client?]" << endl;
-        mq_unlink(queue_path);
-        mq_close(msgq_id);
+    if (msgq_id == -1) {
+        perror("msgget");
         exit(EXIT_FAILURE);
     }
-
-    cout << endl << "[MQ-Connection established]" << endl;
-    time_awaiting = msg.time;
     
-    mq_unlink(queue_path);
-    mq_close(msgq_id);
-    exit(EXIT_SUCCESS);
+    int i;
+    for (i = 0; i < timer; i++) {
+        printf("[Ожидание отклика от клиента: %d секунд из %d]\n", i, timer);
+        if (read_message(msgq_id, &msg, 1) != -1) break;
 
-    for (time_consumed = 0; time_consumed < time_awaiting; time_consumed++) {
-        if (mq_receive(msgq_id, (char *)&msg, recv_size, &sender) == 0) {
-            sleep(time_awaiting);
-        }
-
-//         cout << msg.buf << " | " << time_awaiting << " | " << time_consumed << endl;
-//         time_consumed = 0;
-
-//         if (strcmp(msg.buf, "unlink()") == 0)
-//             break;
+        sleep(1);
     }
+    
+    if (i == timer)
+        printf("[Закрытие...]\n");
+    else {
+        printf("[Сигнал от клиента получен]\n");
+        
+        time_t client_timer = msg.timer;
+        
+        char b[] = "Hi!";
+        send_message(msgq_id, &msg, 2, b, timer);
+        
+        for (i = 0; i < client_timer; i++) {
+            printf("[Ожидание сообщения от клиента: %d секунд из %d]\n", i, (int) client_timer);
+            if (read_message(msgq_id, &msg, 4) != -1) {
+                i = 0;
+                char b[] = "[Server: OK]";
+                send_message(msgq_id, &msg, 3, b, timer);
+                continue;
+            }
 
-    if (time_consumed == time_awaiting)
-        cout << "Client?" << endl;
-    else
-        cout << "Unlink command detected. Terminating..." << endl;
-
-
-    mq_unlink(queue_path);
-    mq_close(msgq_id);
+            sleep(1);
+        }
+        
+        if (i == client_timer)
+            printf("[Клиент? Закрытие...]\n");
+    }
+    
+    msgctl(msgq_id, IPC_RMID, 0);
     exit(EXIT_SUCCESS);
 }
